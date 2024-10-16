035c2c0560a9b5dd48208c68d7482d69bcefe1b7
Vaishnav Verma <123992401+vermavaishnav70@users.noreply.github.com>
2024-10-16 17:16:03 +0530

    Update migration.sql

--- migrations/20241014160345_init/migration.sql
+++ migrations/20241014160345_init/migration.sql
@@ -1,6 +1,7 @@
 -- CreateTable
 CREATE TABLE `MyTable` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-
+    `column1` VARCHAR(191) NOT NULL,
+    UNIQUE INDEX `MyTable_column1_key`(`column1`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
